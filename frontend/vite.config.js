c4881b9401ccc82e38110fee5db86c039e0ff73f
nabilfikrisp <mnabilfikrisp@gmail.com>
2023-09-24 18:56:11 +0700

    resolve conflict

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
->>>>>>> 096e7ab48d3189e333365b7baa051fdc265dc2d2
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   server: {
     watch: {
       usePolling: true,
     },
   },
 });
-=======
-})
->>>>>>> 096e7ab48d3189e333365b7baa051fdc265dc2d2
