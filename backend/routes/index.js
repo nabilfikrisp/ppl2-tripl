b36bdd62def4661179b57471ebf62fea387760d3
nabilfikrisp <mnabilfikrisp@gmail.com>
2023-11-13 12:34:39 +0700

    mock: backend pr 2

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const planRouter = require('./planRoutes');
 const userRouter = require('./userRoutes');
 
 router.get('/', (requrest, response) => {
-  response.send('<h1>Hello Bang</h1>');
+  response.send('<h1>Hello World</h1>');
 });
 
 router.use('/api/auth', authRouter);
