479f90dd48b969e7a351ea13c6b09cb29b1eebcf
nabilfikrisp <mnabilfikrisp@gmail.com>
2023-12-03 18:34:42 +0700

    chore: fix config and add vercel json

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 const { PORT } = process.env;
 // istanbul ignore next: This line is excluded from test coverage
 const MONGODB_URI =
+  // eslint-disable-next-line no-nested-ternary
   process.env.NODE_ENV === 'test'
     ? process.env.MONGODB_URI_UNIT_TEST
-    : process.env.MONGODB_URI_TEST;
+    : process.env.NODE_ENV === 'development'
+    ? process.env.MONGODB_URI_TEST
+    : process.env.MONGODB_URI;
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
